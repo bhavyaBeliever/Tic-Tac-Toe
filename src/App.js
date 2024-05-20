2a8f6c797ef9afecc37f0f2aed562664cb1b25e1
bhavyaBeliever <bhavyashah8443@gmail.com>
2024-05-20 18:27:13 +0530

    Saving the changes for tic-tac-toe

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import React from 'react';
+import { useState } from "react";
+const Square = (value, onSquareClick, isXnext) => {
+  return (
+    <button className="square" value={value} onClick={onSquareClick}>{isXnext ? "x" : "o"} </button>
+  );
+}
+const handleClick = () => {
+  setCountMoves(countMoves+1);
+  
+}
 
 function App() {
+  countMoves, setCountMoves = useState(0)
+  isXnext = (countMoves % 2 === 0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="board">
+
+        <div className="row">
+          <Square value="1" onSquareClick={handleClick} isXnext={isXnext} />
+          <Square value="2" onSquareClick={handleClick} isXnext={isXnext} />
+          <Square value="3" onSquareClick={handleClick} isXnext={isXnext} />
+        </div>
+
+      </div>
+    </>
   );
 }
 
