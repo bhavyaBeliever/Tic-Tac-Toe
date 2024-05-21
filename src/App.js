5276b8b25c63b4b1132928ef410d46d1bb6f7f3b
bhavyaBeliever <bhavyashah8443@gmail.com>
2024-05-21 22:03:56 +0530

    feat:Added winner display code

--- src/App.js
+++ src/App.js
@@ -1,30 +1,94 @@
 import "./App.css"
 import React from 'react';
 import { useState } from "react";
-const Square = (value, onSquareClick, isXnext) => {
+const Square = ({ value, countMoves, setCountMoves, isXnext, history, setHistory }) => {
+  const [click, setClick] = useState(false)
+  const [text, setText] = useState("")
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    if (click) {
+      return;
+    }
+    setClick(true);
+    setCountMoves(countMoves + 1);
+    const newHistory = [...history];
+    newHistory[value] = isXnext ? "X" : "O";
+    setHistory(newHistory);
+    isXnext ? setText("X") : setText("O");
+
+  }
   return (
-    <button className="square" value={value} onClick={onSquareClick}>{isXnext ? "x" : "o"} </button>
+    <button className="square" value={value} onClick={handleClick}>{click && text}</button>
   );
-}
-const handleClick = () => {
-  setCountMoves(countMoves+1);
-  
 }
 
 function App() {
-  countMoves, setCountMoves = useState(0)
-  isXnext = (countMoves % 2 === 0)
+  const [history, setHistory] = useState(Array(9).fill(null))
+  const [countMoves, setCountMoves] = useState(0)
+  const isXnext = (countMoves % 2 === 0)
+  const [end, setEnd] = useState(false);
+  const winnerRow = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+  const isEndGame = (history) => {
+    for (let i = 0; i < winnerRow.length; i++) {
+      const [a, b, c] = winnerRow[i];
+      if (history[a] && history[a] === history[b] && history[a] === history[c]) {
+        console.log(history[a]);
+        return history[a]; // Return the winner ('X' or 'O')
+      }
+    }
+    return null; // Return null if there's no winner
+  };
+  const winner = isEndGame(history);
+  console.log(winner);
   return (
     <>
-      <div className="board">
+      {
+        !winner ? (
+          <>
+            <p>
+              {isXnext ? "X " : "O "}
+              to play
+            </p>
+            <div className="board">
 
-        <div className="row">
-          <Square value="1" onSquareClick={handleClick} isXnext={isXnext} />
-          <Square value="2" onSquareClick={handleClick} isXnext={isXnext} />
-          <Square value="3" onSquareClick={handleClick} isXnext={isXnext} />
-        </div>
+              <div className="row">
+                <Square value="0" countMoves={countMoves} setCountMoves={setCountMoves} isXnext={isXnext} history={history} setHistory={setHistory} />
+                <Square value="1" countMoves={countMoves} setCountMoves={setCountMoves} isXnext={isXnext} history={history} setHistory={setHistory} />
+                <Square value="2" countMoves={countMoves} setCountMoves={setCountMoves} isXnext={isXnext} history={history} setHistory={setHistory} />
+              </div>
 
-      </div>
+              <div className="row ">
+                <Square value="3" countMoves={countMoves} setCountMoves={setCountMoves} isXnext={isXnext} history={history} setHistory={setHistory} />
+                <Square value="4" countMoves={countMoves} setCountMoves={setCountMoves} isXnext={isXnext} history={history} setHistory={setHistory} />
+                <Square value="5" countMoves={countMoves} setCountMoves={setCountMoves} isXnext={isXnext} history={history} setHistory={setHistory} />
+              </div>
+
+              <div className="row">
+                <Square value="6" countMoves={countMoves} setCountMoves={setCountMoves} isXnext={isXnext} history={history} setHistory={setHistory} />
+                <Square value="7" countMoves={countMoves} setCountMoves={setCountMoves} isXnext={isXnext} history={history} setHistory={setHistory} />
+                <Square value="8" countMoves={countMoves} setCountMoves={setCountMoves} isXnext={isXnext} history={history} setHistory={setHistory} />
+              </div>
+
+            </div>
+          </>
+        ) : (
+          <>
+            <p>
+              {`Winner is: ${winner} 🏆`}
+            </p>
+          </>
+        )
+      }
     </>
   );
 }
